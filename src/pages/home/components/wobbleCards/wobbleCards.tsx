ac3250c4f2e070f42370cba255dca0a3fa598bd5
ali-ali18 <alisson.camargo024@gmail.com>
2025-02-09 17:03:54 -0300

    bug fix in WobbleCard

--- src/pages/home/components/wobbleCards/wobbleCards.tsx
+++ src/pages/home/components/wobbleCards/wobbleCards.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 export const WobbleCard = ({
